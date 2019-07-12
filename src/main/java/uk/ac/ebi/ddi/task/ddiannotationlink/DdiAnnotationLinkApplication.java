c3fb66d640b1a159ea9ac94149a40251803fd949
Gaurhari <gaurharidass@gmail.com>
2019-07-12 16:13:13 +0100

    addded new project for annotation tasklet

---
+++ src/main/java/uk/ac/ebi/ddi/task/ddiannotationlink/DdiAnnotationLinkApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.ddi.task.ddiannotationlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdiAnnotationLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdiAnnotationLinkApplication.class, args);
+	}
+
+}

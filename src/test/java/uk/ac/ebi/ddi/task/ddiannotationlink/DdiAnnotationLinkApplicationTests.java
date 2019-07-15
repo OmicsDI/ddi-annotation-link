04b446d1e3a07fb392d126fd6facc4c5e533d931
Gaurhari <gaurharidass@gmail.com>
2019-07-15 11:47:30 +0100

    test for annotation link service

---
+++ src/test/java/uk/ac/ebi/ddi/task/ddiannotationlink/DdiAnnotationLinkApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.ddi.task.ddiannotationlink;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DdiAnnotationLinkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

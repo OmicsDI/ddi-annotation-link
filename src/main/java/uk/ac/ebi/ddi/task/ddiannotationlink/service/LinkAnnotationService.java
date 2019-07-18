db4ee1e143bbff167823e79e78f8d807743e412d
Gaurhari <gaurharidass@gmail.com>
2019-07-18 16:46:55 +0100

    changed to stream and updated to constant value

--- src/main/java/uk/ac/ebi/ddi/task/ddiannotationlink/service/LinkAnnotationService.java
+++ src/main/java/uk/ac/ebi/ddi/task/ddiannotationlink/service/LinkAnnotationService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import uk.ac.ebi.ddi.ddidomaindb.dataset.DSField;
 import uk.ac.ebi.ddi.service.db.model.dataset.Dataset;
 import uk.ac.ebi.ddi.service.db.service.dataset.IDatasetService;
+import uk.ac.ebi.ddi.task.ddiannotationlink.utils.Constants;
 
 import java.util.List;
 import java.util.Set;
@@ -20,7 +21,7 @@ public class LinkAnnotationService {
     }
 
     public void annotateDatasets(String databaseName){
-        getAllDatasetsByDatabase(databaseName).parallelStream().forEach(dt -> {
+        getAllDatasetsByDatabase(databaseName).stream().forEach(dt -> {
             annotateDatasetLink(dt);
                     datasetService.update(dt.getId(), dt);
         });
@@ -28,8 +29,8 @@ public class LinkAnnotationService {
 
     public Dataset annotateDatasetLink(Dataset dataset){
         if (dataset.getAdditional() != null
-                && dataset.getAdditional().containsKey("full_study_link")) {
-            Set<String> links = dataset.getAdditional().remove("full_study_link");
+                && dataset.getAdditional().containsKey(Constants.FULL_STUDY_LINK)) {
+            Set<String> links = dataset.getAdditional().remove(Constants.FULL_STUDY_LINK);
             dataset.getAdditional().put(DSField.Additional.LINK.getName(), links);
         }
         return dataset;
